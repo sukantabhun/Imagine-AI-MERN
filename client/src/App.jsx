21ae9de3829dbd4f4528cad74ca4bb3aa8987ac0
Sukanta Bhunia <113849325+sukantabhun@users.noreply.github.com>
2024-11-15 15:19:48 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,8 +15,11 @@ const App = () => {
             <h1 className='text-[22px] font-extrabold'>Imagine AI</h1>
           </div>
         </Link>
-
-        <Link to="/create-post" className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md">Create</Link>
+        <div>
+          <Link to='https://ai-studio-project.vercel.app/' className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md">Home</Link>
+          <Link to="/create-post" className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md">Create</Link>
+        </div>
+        
       </header>
       <main className='sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]'>
         <Routes>
