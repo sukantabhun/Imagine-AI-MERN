66795d0ff51b3339f240e6af19e60844d5704230
Sukanta Bhunia <113849325+sukantabhun@users.noreply.github.com>
2024-11-16 19:21:34 +0530

    Update Card.jsx

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,12 +1,47 @@
-import React from 'react'
+import React from 'react';
 
-import { download } from '../assets'
-import { downloadImage} from '../utils'
+import { download } from '../assets';
+import { downloadImage } from '../utils';
 
-const Card = ({_id ,name ,prompt ,photo}) => {
+const Card = ({ _id, name, prompt, photo }) => {
+  // Capitalize the first letter
+  const capitalizedFirstLetter = name[0].toUpperCase();
+
+  // Set background color based on the first letter
+  const getBackgroundColor = (initial) => {
+    switch (initial) {
+      case 'A':
+      case 'B':
+      case 'C':
+        return 'bg-red-500';
+      case 'D':
+      case 'E':
+      case 'F':
+        return 'bg-blue-500';
+      case 'G':
+      case 'H':
+      case 'I':
+        return 'bg-yellow-500';
+      case 'J':
+      case 'K':
+      case 'L':
+        return 'bg-purple-500';
+      case 'M':
+      case 'N':
+      case 'O':
+        return 'bg-green-500';
+      case 'P':
+      case 'Q':
+      case 'R':
+        return 'bg-orange-500';
+      default:
+        return 'bg-gray-500';
+    }
+  };
+
   return (
-    <div className='rounded-xl group relative shadow-card hower:shadow-cardhover card'>
-      <img 
+    <div className='rounded-xl group relative shadow-card hover:shadow-cardhover card'>
+      <img
         className='w-full h-auto object-cover rounded-xl'
         src={photo}
         alt={prompt}
@@ -14,23 +49,27 @@ const Card = ({_id ,name ,prompt ,photo}) => {
       <div className='group-hover:flex flex-col max-h-[94.5%] hidden absolute bottom-0 left-0 right-0 bg-[#10131f] m-2 p-4 rounded-md'>
         <p className='text-white text-md overflow-y-auto prompt'>{prompt}</p>
         <div className='mt-5 flex justify-between items-center gap-2'>
-            <div className='flex items-center gap-2'>
-              <div className='w-7 h-7 rounded-full object-cover bg-green-700 flex justify-center items-center text-white text-xs font-bold'>
-                {name[0]}
-              </div>
-              <p className='text-white text-sm'>{name}</p>
-            </div>
-            <button
-              type='button'
-              onClick={()=> downloadImage(_id,photo)}
-              className='outline-none bg-transparent border-none'
+          <div className='flex items-center gap-2'>
+            <div
+              className={`w-7 h-7 rounded-full object-cover ${getBackgroundColor(
+                capitalizedFirstLetter
+              )} flex justify-center items-center text-white text-xs font-bold`}
             >
-              <img src={download} alt='download' class = 'h-6 w-6 object-contain invert' />
-            </button>
+              {capitalizedFirstLetter}
+            </div>
+            <p className='text-white text-sm'>{name}</p>
+          </div>
+          <button
+            type='button'
+            onClick={() => downloadImage(_id, photo)}
+            className='outline-none bg-transparent border-none'
+          >
+            <img src={download} alt='download' className='h-6 w-6 object-contain invert' />
+          </button>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
